6249698be03063b5b775053ce5e0c2e704175b9a
Daryl <daryl.n.w.k@gmail.com>
2015-04-07 01:35:04 +0800

    Minor fixed in CSS

--- public/modules/wiki/controllers/wiki-app.js
+++
@@ -1,6 +0,0 @@
-//Define an angular module for our app
-'use strict';
-
-// angular.module('wiki').controller('rightbar', function($scope, $rootScope) {
-// 	$scope.mod = $rootScope.selectedModule;
-// });
