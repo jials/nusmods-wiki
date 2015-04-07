19b5119aacb24672f4c5dc535de6771d228505cd
Daryl <daryl.n.w.k@gmail.com>
2015-04-08 01:51:58 +0800

    Added smooth scroll
    Haven't properly format...

--- public/modules/wiki/controllers/wiki.client.controller.js
+++ public/modules/wiki/controllers/wiki.client.controller.js
@@ -4,5 +4,13 @@
 angular.module('wiki').controller('WikiController', ['$scope', '$stateParams', '$location',
 	function($scope, $stateParams, $location) {
 		$scope.moduleTitle = $location.path().substr(1);
+
+		$scope.isActive = function(item) {
+			if (item == $location.hash()) {
+				return true;
+			}
+
+			return false;
+		};
 	}
 ]);
