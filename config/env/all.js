f51a90e83684d7cd72285a50aaf15cb8ae45dc80
jiale <jiale@comp.nus.edu.sg>
2015-04-14 09:46:00 +0800

    changed the app title

--- config/env/all.js
+++ config/env/all.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	app: {
-		title: 'MEAN.JS',
+		title: 'NUS Modules Wiki',
 		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
 		keywords: 'mongodb, express, angularjs, node.js, mongoose, passport'
 	},
--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
 		}
 	},
 	app: {
-		title: 'MEAN.JS - Development Environment'
+		title: 'NUS Modules Wiki - Development Environment'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
