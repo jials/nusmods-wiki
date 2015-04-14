9f3fb4c33a4ddea8a86b0c2b2d7b27457405cbaa
jiale <jiale@comp.nus.edu.sg>
2015-04-14 10:39:10 +0800

    fixed the bug that doesnt allow user to edit after login

--- public/modules/wiki/controllers/wiki.client.controller.js
+++ public/modules/wiki/controllers/wiki.client.controller.js
@@ -2,8 +2,10 @@
 
 // Wiki controller
 angular.module('wiki')
-.controller('WikiController', ['$scope', '$stateParams', '$location',
-	function($scope, $stateParams, $location) {
+.controller('WikiController', ['$scope', '$stateParams', '$location', 'Authentication',
+	function($scope, $stateParams, $location, Authentication) {
+		$scope.authentication = Authentication;
+
 		$scope.moduleTitle = $stateParams.moduleTitle;
 
 		$scope.isActive = function(item) {
