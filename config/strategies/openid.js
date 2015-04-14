98a716ee9102ac2c9483b6b6b7834ea4a2c10876
jiale <jiale@comp.nus.edu.sg>
2015-04-14 09:33:18 +0800

    changed the return url

--- config/strategies/openid.js
+++ config/strategies/openid.js
@@ -6,13 +6,14 @@
 var passport = require('passport'),
 	OpenIDStrategy = require('passport-openid').Strategy,
 	config = require('../config'),
+    server = require('../server'),
     users = require('../../app/controllers/users.server.controller');
   
 module.exports = function() {
     passport.use(new OpenIDStrategy({
             providerURL: 'https://openid.nus.edu.sg/',
-            returnURL: 'http://localhost:3000/auth/openid/return',
-            realm: 'http://localhost:3000/',
+            returnURL: 'http://' + server.host + '/auth/openid/return',
+            realm: 'http://' + server.host + '/',
             profile: true
         },
     
