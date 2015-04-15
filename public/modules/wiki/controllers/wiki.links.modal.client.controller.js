b6b9df92301426a4ceac06760f9894e828284b91
Daryl <daryl.n.w.k@gmail.com>
2015-04-16 02:03:31 +0800

    Added links form validation

--- public/modules/wiki/controllers/wiki.links.modal.client.controller.js
+++ public/modules/wiki/controllers/wiki.links.modal.client.controller.js
@@ -2,24 +2,24 @@
 
 angular.module('wiki').controller('LinksModalCtrl', function ($scope, $modal, $log) {
 
-  $scope.home = 'www.nus.edu.sg';
-  $scope.facebook = 'www.facebook.com';
+	$scope.home = 'www.nus.edu.sg';
+	$scope.facebook = 'www.facebook.com';
 
-  $scope.links = [$scope.home, $scope.facebook];
+	$scope.links = [$scope.home, $scope.facebook];
 
-  $scope.open = function (size) {
+	$scope.open = function (size) {
 
-    var modalInstance = $modal.open({
-      templateUrl: 'LinksModalContent.html',
-      controller: 'LinksModalInstanceCtrl',
-      size: size,
-      resolve: {
-        links: function () {
-          return $scope.links;
-        }
-      }
-    });
-  };
+		var modalInstance = $modal.open({
+			templateUrl: 'LinksModalContent.html',
+			controller: 'LinksModalInstanceCtrl',
+			size: size,
+			resolve: {
+				links: function () {
+					return $scope.links;
+				}
+			}
+		});
+	};
 });
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
@@ -27,14 +27,66 @@ angular.module('wiki').controller('LinksModalCtrl', function ($scope, $modal, $l
 
 angular.module('wiki').controller('LinksModalInstanceCtrl', function ($scope, $modalInstance, links) {
 
-  $scope.items = links;
+	// $scope.items = links;
 
-  $scope.save = function () {
-    alert('saving ' + $scope.items[0] + ' ' + $scope.items[1]);
-    $modalInstance.close();
-  };
+	$scope.links = { homepage : '', facebook: '' };
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
+	$scope.save = function () {
+		$scope.$broadcast('show-errors-check-validity');
+  		if ($scope.linksForm.$invalid && (!$scope.disabledHomepage || !$scope.disabledFacebook)) { return; }
+
+		alert('saving ' + JSON.stringify($scope.links) + ' ' + $scope.semester);
+		$modalInstance.close();
+	};
+
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
+});
+
+angular.module('wiki').directive('httpPrefix', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, controller) {
+            function ensureHttpPrefix(value) {
+                // Need to add prefix if we don't have http:// prefix already AND we don't have part of it
+                if(value && !/^(http):\/\//i.test(value)
+                   && 'http://'.indexOf(value) === -1) {
+                    controller.$setViewValue('http://' + value);
+                    controller.$render();
+                    return 'http://' + value;
+                }
+                else
+                    return value;
+            }
+            controller.$formatters.push(ensureHttpPrefix);
+            controller.$parsers.splice(0, 0, ensureHttpPrefix);
+        }
+    };
+});
+
+angular.module('wiki').directive('showErrors', function() {
+	return {
+		restrict: 'A',
+		require:  '^form',
+		link: function (scope, el, attrs, formCtrl) {
+	        // find the text box element, which has the 'name' attribute
+	        var inputEl   = el[0].querySelector("[name]");
+	        // convert the native text box element to an angular element
+	        var inputNgEl = angular.element(inputEl);
+	        // get the name on the text box so we know the property to check
+	        // on the form controller
+	        var inputName = inputNgEl.attr('name');
+
+	        scope.$on('show-errors-check-validity', function() {
+				el.toggleClass('has-error', formCtrl[inputName].$invalid);
+			});
+
+	        // only apply the has-error class after the user leaves the text box
+	        inputNgEl.bind('blur', function() {
+	        	el.toggleClass('has-error', formCtrl[inputName].$invalid);
+	        })
+	    }
+	}
 });
