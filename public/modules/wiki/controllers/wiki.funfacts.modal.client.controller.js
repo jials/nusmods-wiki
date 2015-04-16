4e83a634c64c4128854cc8fd808409a14fb39649
Daryl <daryl.n.w.k@gmail.com>
2015-04-16 21:45:34 +0800

    Fixed funfacts controller

--- public/modules/wiki/controllers/wiki.funfacts.modal.client.controller.js
+++ public/modules/wiki/controllers/wiki.funfacts.modal.client.controller.js
@@ -1,43 +1,39 @@
 'use strict';
 
-angular.module('wiki').controller('FunFactsModalCtrl', function ($scope, $modal, $log) {
+angular.module('wiki').controller('FunFactsModalCtrl', function ($scope, $modal, $log, $http, $stateParams, Authentication) {
 
-  $scope.content = 'This is fun facts content';
-
-  $scope.open = function (size) {
-
-    var modalInstance = $modal.open({
-      templateUrl: 'FunFactsModalContent.html',
-      controller: 'FunFactsModalInstanceCtrl',
-      size: size,
-      resolve: {
-        content: function () {
-          return $scope.content;
-        }
-      }
+	$http.get('/' + $stateParams.moduleTitle).success(function(data){
+        $scope.content = data.funFacts[data.funFacts.length - 1].content;
     });
 
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      // $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
+	$scope.open = function (size) {
+
+		var modalInstance = $modal.open({
+			templateUrl: 'FunFactsModalContent.html',
+			controller: 'FunFactsModalInstanceCtrl',
+			size: size,
+			resolve: {
+				content: function () {
+					return $scope.content;
+				}
+			}
+		});
+	};
 });
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
 
-angular.module('wiki').controller('FunFactsModalInstanceCtrl', function ($scope, $modalInstance, content) {
+angular.module('wiki').controller('FunFactsModalInstanceCtrl', function ($scope, $modalInstance, content, $http, $stateParams, Authentication) {
+	$scope.content = content;
 
-  $scope.content = content;
+    $scope.save = function () {
+        $http.put('/' + $stateParams.moduleTitle, {editedBy: Authentication.user.id, type: 'funFacts', funFacts: $scope.content}).success(function(data){     
+        });
+        $modalInstance.close();
+    };
 
-  $scope.save = function () {
-    alert('saving ' + $scope.content);
-    $modalInstance.close();
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
 });
