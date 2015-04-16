dc2b02c7545cb11b5ab9d8adec666fcd1e4cff57
Daryl <daryl.n.w.k@gmail.com>
2015-04-16 17:25:40 +0800

    Show loader on http request

--- public/modules/wiki/controllers/wiki.maincontent.client.controller.js
+++ public/modules/wiki/controllers/wiki.maincontent.client.controller.js
@@ -1,9 +1,14 @@
 'use strict';
 
-angular.module('wiki').controller('MainContentCtrl', ['$scope', '$http', '$stateParams',
-	function($scope, $http, $stateParams) {
+angular.module('wiki').controller('MainContentCtrl', ['$scope', '$http', '$stateParams', '$timeout',
+	function($scope, $http, $stateParams, $timeout) {
+        $scope.showSpinner = true;
+
 		$http.get('http://api.nusmods.com/2014-2015/modules/' + $stateParams.moduleTitle + '/index.json?callback=?').success(function(data){
 			$scope.module = data;
+            $timeout(function() {
+                $scope.showSpinner = false;
+            }, 3000);
 		});
 	}
 ]);
