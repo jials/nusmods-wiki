1e9eac3de6f32e3002183fd66f2c53210ad6b17f
Daryl <daryl.n.w.k@gmail.com>
2015-04-17 21:03:49 +0800

    Fixed jshint warnings

--- public/modules/core/directives/core.loader.client.directives.js
+++ public/modules/core/directives/core.loader.client.directives.js
@@ -3,11 +3,11 @@
 angular.module('core').directive('stateLoadingIndicator', function($rootScope) {
   return {
     restrict: 'E',
-    template: "<div ng-show='isStateLoading' class='loading-indicator'>" +
-    "<div class='loading-indicator-body' style='z-index:9999;background-color:#FFFFFF;position:absolute;width:100%;height:100%;'>" +
-    "<div class='spinner'><rotating-plane-spinner></rotating-plane-spinner></div>" +
-    "</div>" +
-    "</div>",
+    template: '<div ng-show="isStateLoading" class="loading-indicator">' +
+    '<div class="loading-indicator-body" style="z-index:9999;background-color:#FFFFFF;position:absolute;width:100%;height:100%;">' +
+    '<div class="spinner"><rotating-plane-spinner></rotating-plane-spinner></div>' +
+    '</div>' +
+    '</div>',
     replace: true,
     link: function(scope, elem, attrs) {
       scope.isStateLoading = false;
