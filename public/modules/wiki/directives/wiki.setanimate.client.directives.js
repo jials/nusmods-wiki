86b98807ece6090dc46b3b5c80021e669364b5cb
Daryl <daryl.n.w.k@gmail.com>
2015-04-17 21:01:09 +0800

    Disable animate on carousel

---
+++ public/modules/wiki/directives/wiki.setanimate.client.directives.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('wiki')
+.directive('setNgAnimate', ['$animate', function ($animate) {
+    return {
+        link: function ($scope, $element, $attrs) {
+            $scope.$watch( function() {
+                return $scope.$eval($attrs.setNgAnimate, $scope);
+            }, function(valnew, valold){
+                console.log('Directive animation Enabled: ' + valnew);
+                $animate.enabled(!!valnew, $element);
+            });
+        }
+    };
+}]);
