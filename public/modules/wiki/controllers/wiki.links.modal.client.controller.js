e7e1df7d3c6b2065f20ac3b6813f64eae5a6d105
Daryl <daryl.n.w.k@gmail.com>
2015-04-18 15:18:29 +0800

    Fixed url validation

--- public/modules/wiki/controllers/wiki.links.modal.client.controller.js
+++ public/modules/wiki/controllers/wiki.links.modal.client.controller.js
@@ -95,7 +95,7 @@ angular.module('wiki').directive('httpPrefix', function() {
         link: function(scope, element, attrs, controller) {
             function ensureHttpPrefix(value) {
                 // Need to add prefix if we don't have http:// prefix already AND we don't have part of it
-                if (value && !/^(http):\/\//i.test(value) && ('http://').indexOf(value) === -1) {
+                if (value && !(/^(http):\/\//i).test(value) && ('http://').indexOf(value) === -1) {
                     controller.$setViewValue('http://' + value);
                     controller.$render();
                     return 'http://' + value;
