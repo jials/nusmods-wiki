a81a6739b5f567544008e65f7a26bcabec776b3e
Daryl <daryl.n.w.k@gmail.com>
2015-04-18 14:51:36 +0800

    Fixed rightbar

--- public/modules/wiki/controllers/wiki.rightbar.content.client.controller.js
+++ public/modules/wiki/controllers/wiki.rightbar.content.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('wiki').controller('RightBarContentCtrl', [ '$scope',
-	function ( $scope ) {
+angular.module('wiki').controller('RightBarContentCtrl', [ '$scope', '$http', '$stateParams',
+	function ($scope, $http, $stateParams) {
 		$scope.data = {
 			selectedIndex: 0,
 			secondLocked:  true,
@@ -14,5 +14,20 @@ angular.module('wiki').controller('RightBarContentCtrl', [ '$scope',
 		$scope.previous = function() {
 			$scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
 		};
+
+
+		$http.get('http://api.nusmods.com/2014-2015/moduleList.json?callback=?').success(function(data) {
+			// alert(JSON.stringify(data));
+
+			var semester;
+			for (var i = 0; i < data.length; i++) {
+				if (data[i].ModuleCode == $stateParams.moduleTitle) {
+					semester = data[i].Semesters;
+				}
+			}
+
+			$scope.semesterOneDisabled = (semester.indexOf(1) > -1) ? false : true;
+			$scope.semesterTwoDisabled = (semester.indexOf(2) > -1) ? false : true;
+    	});
 	}
 ]);
