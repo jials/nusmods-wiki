13e7e5a395a26bb3a85af6bf4c52c8ba54c953b0
Daryl <daryl.n.w.k@gmail.com>
2015-04-19 01:51:22 +0800

    Fixed core controller syntax

--- public/modules/core/directives/core.loader.client.directives.js
+++ public/modules/core/directives/core.loader.client.directives.js
@@ -1,23 +1,25 @@
 'use strict';
 
-angular.module('core').directive('stateLoadingIndicator', function($rootScope) {
-  return {
-    restrict: 'E',
-    template: '<div ng-show="isStateLoading" class="loading-indicator">' +
-    '<div class="loading-indicator-body" style="z-index:9999;background-color:#FFFFFF;position:absolute;width:100%;height:100%;">' +
-    '<div class="spinner"><rotating-plane-spinner></rotating-plane-spinner></div>' +
-    '</div>' +
-    '</div>',
-    replace: true,
-    link: function(scope, elem, attrs) {
-      scope.isStateLoading = false;
- 
-      $rootScope.$on('$stateChangeStart', function() {
-        scope.isStateLoading = true;
-      });
-      $rootScope.$on('$stateChangeSuccess', function() {
-        scope.isStateLoading = false;
-      });
-    }
-  };
-});
+angular.module('core').directive('stateLoadingIndicator', [ '$rootScope',
+	function($rootScope) {
+		return {
+			restrict: 'E',
+			template: '<div ng-show="isStateLoading" class="loading-indicator">' +
+			'<div class="loading-indicator-body" style="z-index:9999;background-color:#FFFFFF;position:absolute;width:100%;height:100%;">' +
+			'<div class="spinner"><rotating-plane-spinner></rotating-plane-spinner></div>' +
+			'</div>' +
+			'</div>',
+			replace: true,
+			link: function(scope, elem, attrs) {
+				scope.isStateLoading = false;
+
+				$rootScope.$on('$stateChangeStart', function() {
+					scope.isStateLoading = true;
+				});
+				$rootScope.$on('$stateChangeSuccess', function() {
+					scope.isStateLoading = false;
+				});
+			}
+		};
+	}
+]);
