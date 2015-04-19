0d677e2df56f8c3ff1ac35999075db3e2c02e839
Daryl <daryl.n.w.k@gmail.com>
2015-04-19 20:35:26 +0800

    Added proxy

--- public/modules/core/controllers/search.client.controller.js
+++ public/modules/core/controllers/search.client.controller.js
@@ -3,7 +3,7 @@
 angular.module('core').controller('SearchController', ['$scope', '$http', '$location',
 	function($scope, $http, $location) {
 		function getModules() {
-			$http.get('http://api.nusmods.com/2014-2015/moduleList.json?callback=?').success(function(data, status, headers, config) {
+			$http.get('/getmodules').success(function(data, status, headers, config) {
 				$scope.modules = data;
 			}).
 		    error(function(data) {
